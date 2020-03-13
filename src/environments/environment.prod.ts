edf6e404fe2ff6f0d87fcf3f1d38ad56bf1c4d5c
Filipe Petitemberte <filipe.petitemberte@gmail.com>
2020-03-13 12:38:03 -0300

    env.prod updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB_XRIEjZI1oNWigNZNqPSlHqrJu0coQkM',
+    authDomain: 'challenge-afefa.firebaseapp.com',
+    databaseURL: 'https://challenge-afefa.firebaseio.com',
+    projectId: 'challenge-afefa',
+    storageBucket: 'challenge-afefa.appspot.com',
+    messagingSenderId: '937052825128',
+    appId: '1:937052825128:web:7e8b3bf641a9d89513371d',
+    measurementId: 'G-B1BZ1TCR4T'
+  }
 };
