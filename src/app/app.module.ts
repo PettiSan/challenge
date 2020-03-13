836766b466ccf7a9b5e18f855c4c416ae3b3b99c
Filipe Petitemberte <filipe.petitemberte@gmail.com>
2020-03-13 03:46:58 -0300

    firabese authetication & hosting setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,34 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatBadgeModule } from '@angular/material/badge';
 import { ScheduleComponent } from './pages/schedule/schedule/schedule.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { RestaurantsListComponent } from './components/restaurants-list/restaurants-list.component';
 
+import { DragAndDropModule } from 'angular-draggable-droppable';
+import { MomentFromNowPipe } from './pipes/moment-from-now.pipe';
+
+// seta a language para Portugues para o AngularCalendar
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
+registerLocaleData(localePt);
+
+
+// firebase settings
+const firebaseConfig = {
+  apiKey: "AIzaSyB_XRIEjZI1oNWigNZNqPSlHqrJu0coQkM",
+  authDomain: "challenge-afefa.firebaseapp.com",
+  databaseURL: "https://challenge-afefa.firebaseio.com",
+  projectId: "challenge-afefa",
+  storageBucket: "challenge-afefa.appspot.com",
+  messagingSenderId: "937052825128",
+  appId: "1:937052825128:web:7e8b3bf641a9d89513371d",
+  measurementId: "G-B1BZ1TCR4T"
+};
 
 export function momentAdapterFactory() {
   return adapterFactory(moment);
@@ -25,6 +51,11 @@ export function momentAdapterFactory() {
   declarations: [
     AppComponent,
     ScheduleComponent,
+    LoginComponent,
+    CalendarComponent,
+    UserInfoComponent,
+    RestaurantsListComponent,
+    MomentFromNowPipe,
   ],
   imports: [
     BrowserModule,
@@ -35,6 +66,8 @@ export function momentAdapterFactory() {
     MatInputModule,
     MatIconModule,
     MatDividerModule,
+    MatBadgeModule,
+    DragAndDropModule,
     CalendarModule.forRoot({ provide: DateAdapter, useFactory: momentAdapterFactory })
   ],
   exports: [
@@ -43,8 +76,11 @@ export function momentAdapterFactory() {
     MatInputModule,
     MatIconModule,
     MatDividerModule,
+    MatBadgeModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
