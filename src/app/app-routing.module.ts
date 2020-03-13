fb430843efa92472a6b59d7d2c67a3880978b672
Filipe Petitemberte <filipe.petitemberte@gmail.com>
2020-03-13 03:47:43 -0300

    login component add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ScheduleComponent } from './pages/schedule/schedule/schedule.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '404', component: LoginComponent },
   { path: 'schedule', component: ScheduleComponent },
 ];
 
