3933088f4b52e8cd368d89ca7b8117932b47a42b
Filipe Petitemberte <filipe.petitemberte@gmail.com>
2020-03-18 19:27:42 -0300

    login component firebase authetication updated

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/providers/api.service';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,18 +9,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  userLoginForm = this.fb.group({
+    email: ['usuario.1@gmail.com', Validators.required],
+    password: ['123456', [Validators.required, Validators.minLength(3)]],
+  });
+  formPending: boolean = false;
 
-  constructor(private apiService: ApiService, private router: Router) { }
+  constructor(private apiService: ApiService, private router: Router, private fb: FormBuilder) { }
 
-  ngOnInit(): void {
-    this.apiService.login('usuario.1@gmail.com', '123456')
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.formPending = true;
+
+    this.apiService.login(this.userLoginForm.controls.email.value, this.userLoginForm.controls.password.value)
       .then(loginRes => {
+        const res: any = loginRes;
 
-        this.router.navigate(['schedule']);
+        if (res.operationType === 'signIn') {
+          this.router.navigate(['schedule']);
+        } else {
+          console.log(res);
+        }
       })
       .catch(loginError => {
         console.log(loginError);
-      });
+      })
+      .finally(() => {this.formPending = false; }
+    );
   }
-
 }
